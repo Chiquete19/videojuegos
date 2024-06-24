dfc50eb06524a551cb3021f343cf0228281abbd5
Fernando Chiquete <ferchiquete2003@gmail.com>
2024-06-23 19:07:20 -0700

    Agregando index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+    <h2>
+        <?php 
+            $server="127.0.0.1";$user="root";$pass="";$db="videojuegos";
+            $conn=mysqli_connect($server,$user,$pass,$db);
+
+
+
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+                echo "Connected successfully";
+              
+            $query="Select * from cliente";
+            $results=$conn->query($query);
+            if ($results->num_rows > 0) {
+                // output data of each row
+                while($row = $results->fetch_assoc()) {
+                  echo "<br> id: " . $row["clave"]. " - nombre: " . $row["nombre"]. "- Direccion " . $row["direccion"]. "<br>";
+                }
+              } else {
+                echo "0 results";
+              }
+              $conn->close();
+
+        ?>
+    </h2>
+</body>
+</html>
